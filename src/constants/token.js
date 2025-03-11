561e4632f0279c5c28e6bdeef026c25c0808b692
Marko <129887087+Marko010101@users.noreply.github.com>
2025-03-11 23:01:03 +0400

    Hotfix: Changed token

--- src/constants/token.js
+++ src/constants/token.js
@@ -1,1 +1,2 @@
-export const token = "9d101d97-c2a3-4004-9fca-215ed2391c10";
+// export const token = "9d101d97-c2a3-4004-9fca-215ed2391c10";
+export const token = "9e689186-f15c-40f4-8f6a-3f88261fc893";
